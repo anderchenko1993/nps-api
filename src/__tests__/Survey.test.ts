9a335744278694fc7f6f81ff21533d764b936cef
“Anderson <anderson_chenko@hotmail.com>
2021-03-05 00:17:03 -0300

    implementado response dos erros de forma customizada

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -1,14 +1,21 @@
 import request from 'supertest';
 import { app } from '../app';
 
-import createConnection from "../database";
+import { getConnection } from 'typeorm';
 
+import createConnection from '../database';
 
 describe("Surveys", () => {
     beforeAll(async () => {
         const connection = await createConnection();
         await connection.runMigrations(); 
     });
+
+    // afterAll(async () => {
+    //     const connection = getConnection();
+    //     connection.dropDatabase();
+    //     connection.close();
+    // });
 
     it("Should be able to create a new survey", async() => {
         const response = await request(app).post("/surveys").send({
